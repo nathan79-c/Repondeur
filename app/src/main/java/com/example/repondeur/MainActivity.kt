09e856ffd0da57c2579614b75b11dfa5dee62b13
nathan79-c <19kt210@esissalma.org>
2024-09-14 16:14:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/repondeur/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.repondeur
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.repondeur.ui.theme.RepondeurTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RepondeurTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    RepondeurTheme {
+        Greeting("Android")
+    }
+}
+
+@Preview
+@Composable
+fun GreetingPreviewDark() {
+    RepondeurTheme(darkTheme = true) {
+        Greeting("Android")
+    }
+
+}
