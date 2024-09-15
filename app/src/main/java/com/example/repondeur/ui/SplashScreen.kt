bf1e06399097ed603bc5ec9e4f119fbd4abf63be
nathan79-c <19kt210@esissalma.org>
2024-09-16 00:14:50 +0200

    create splash screen

--- app/src/main/java/com/example/repondeur/ui/SplashScreen.kt
+++ app/src/main/java/com/example/repondeur/ui/SplashScreen.kt
@@ -1,16 +1,28 @@
 package com.example.repondeur.ui
 
 import android.window.SplashScreen
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.repondeur.R
 import kotlinx.coroutines.delay
 
 @Composable
@@ -29,7 +41,19 @@ fun SplashScreen(onTimeout: () -> Unit){
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-
+            Image(
+                painter = painterResource(R.drawable.play_store_512),
+                contentDescription = "Logo of this aplication",
+                modifier = Modifier
+                    .size(120.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = stringResource(R.string.app_name),
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.onSurface
+            )
         }
     }
 }
