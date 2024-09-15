0db591777aa5b13242e0aea7cf17f96edf3652a5
nathan79-c <19kt210@esissalma.org>
2024-09-15 23:58:04 +0200

    build the splash screen

--- app/src/main/java/com/example/repondeur/ui/SplashScreen.kt
+++ app/src/main/java/com/example/repondeur/ui/SplashScreen.kt
@@ -1,2 +1,35 @@
 package com.example.repondeur.ui
 
+import android.window.SplashScreen
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(onTimeout: () -> Unit){
+    LaunchedEffect(Unit) {
+        delay(2000)
+        onTimeout()
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment =  Alignment.Center
+    ){
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+        }
+    }
+}
